bec02ac541357b559711a1bc938aa9775cc48561
ChaochengRen <lensnikor@gmail.com>
2018-08-12 22:10:18 +0800

    sync

---
+++ src/template/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="app">
+        <h1>{{msg}}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                msg: "Hello!"
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
